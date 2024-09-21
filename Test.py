8ac06316b2311dff537eefc214f1f903dbb74b93
ChangReily <45777474+ChangReily@users.noreply.github.com>
2024-09-21 22:15:51 +0800

    Update Test.py

--- Test.py
+++ Test.py
@@ -45,3 +45,6 @@ if __name__ == '__main__':
     print(OpenDayList)
     print(OpenDayList)
     print(OpenDayList)
+    output_file = os.getenv('GITHUB_OUTPUT')  
+    with open(output_file, "a") as myfile:
+        myfile.write(f"changelog={OpenDayList}")
